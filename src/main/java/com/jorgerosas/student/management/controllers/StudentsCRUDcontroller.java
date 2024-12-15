6759bb437401cbb6095a2550f4a5177c584aa0d6
jorge Rosas <jorge.99rg@gmail.com>
2024-12-15 07:14:08 -0600

    Student controller finished

--- src/main/java/com/jorgerosas/student/management/controllers/StudentsCRUDcontroller.java
+++ src/main/java/com/jorgerosas/student/management/controllers/StudentsCRUDcontroller.java
@@ -1,12 +1,73 @@
 package com.jorgerosas.student.management.controllers;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.jorgerosas.student.management.domain.Student;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/students")
 public class StudentsCRUDcontroller {
 
+    private List<Student> students = new ArrayList<Student>(Arrays.asList(
+            new Student(1, 20, "Alejandro García", "alejandro.garcia@example.com", "Computer Science"),
+            new Student(2, 22, "María López", "maria.lopez@example.com", "Medicine"),
+            new Student(3, 19, "Carlos Martínez", "carlos.martinez@example.com", "Law"),
+            new Student(4, 21, "Sofía Fernández", "sofia.fernandez@example.com", "Architecture")
+    ));
 
+    @GetMapping
+    public List<Student> getAllStudents(){return students;}
+
+    @GetMapping("/{email}")
+    public Student getStudentbyEmail(@PathVariable String email){
+        for(Student s : students){
+            if(s.getEmail().equals(email)) return s;
+        }
+        return null;
+    }
+
+    @PostMapping
+    public void postStudent(@RequestBody Student student){ students.add(student); }
+
+    @PutMapping
+    public void putStudent(@RequestBody Student student){
+        for(Student s : students){
+            if(s.getID() == student.getID()){
+                s.setAge(student.getAge());
+                s.setName(student.getName());
+                s.setEmail(student.getEmail());
+                s.setCourse(student.getCourse());
+            }
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteStudentById(@PathVariable int id){
+        for(Student s : students) {
+            if (s.getID() == id) students.remove(s);
+        }
+    }
+
+    @PatchMapping
+    public void patchStudent(@RequestBody Student student){
+        for(Student s : students){
+            if(s.getID() == student.getID()){
+                if(student.getAge()!=0){
+                    s.setAge(student.getAge());
+                }
+                if(student.getName()!=null){
+                    s.setName(student.getName());
+                }
+                if(student.getEmail()!=null){
+                    s.setEmail(student.getEmail());
+                }
+                if(student.getCourse()!=null){
+                    s.setCourse(student.getCourse());
+                }
+            }
+        }
+    }
 }
--- src/main/java/com/jorgerosas/student/management/domain/Student.java
+++ src/main/java/com/jorgerosas/student/management/domain/Student.java
@@ -1,4 +1,54 @@
 package com.jorgerosas.student.management.domain;
 
 public class Student {
+    private int ID, age;
+    private String name, email, course;
+
+    public Student(int ID, int age, String name, String email, String course) {
+        this.ID = ID;
+        this.age = age;
+        this.name = name;
+        this.email = email;
+        this.course = course;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
 }
